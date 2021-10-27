7f778dd9e94844eab4a9930294500456c24af3db
Imarinize <marina.izumikawa3@askul.com>
2021-10-27 21:04:09 +0900

    first commit

---
+++ src/test/kotlin/com/example/cypher/CypherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cypher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CypherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
